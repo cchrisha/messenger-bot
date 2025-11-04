586c3fba856ed7dc44c6d43a473b736522d34670
chrisha <153200985+cchrisha@users.noreply.github.com>
2025-11-04 18:44:58 +0800

    pagbasa failed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import e from "express";
 import express from "express";
 import fetch from "node-fetch";
 
@@ -10,6 +11,8 @@ const PAGE_ACCESS_TOKEN = "EAAddxc7RK0EBP3gh29JgZBK7QkCLiMsZA2QCHkQvwAVZAuQ8qnHQ
 //temporary user progress tracker
 const userProgress = {};
 
+//---------------------------------------------------------------------//
+//-------------------FUNCTIONS----------------------------------------//
 // webhook verification
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
@@ -50,6 +53,11 @@ app.post("/webhook", async (req, res) => {
         }
       }
 
+      if (event.message && event.message.text && userProgress[sender_psid] === "WAITING_OPINIONATED_ANSWER") {
+        await handleOpinionatedAnswer(sender_psid);
+        delete userProgress[sender_psid]; // clear progress after answering
+      }
+
       // handle button clicks (postbacks)
       if (event.postback) {
         const payload = event.postback.payload;
@@ -60,77 +68,18 @@ app.post("/webhook", async (req, res) => {
           await sendNextActivity(sender_psid);
         } else if (payload === "SAAN_PO") {
           await sendParabulaLesson(sender_psid);
-        } else if (payload === "UNDERSTOOD") {
-          await sendMessage(sender_psid, "✅ Okay, sige!");
-          await new Promise((r) => setTimeout(r, 1500));
-
-        //   const quizPayload = {
-        //     recipient: { id: sender_psid },
-        //     message: {
-        //       attachment: {
-        //         type: "template",
-        //         payload: {
-        //           template_type: "button",
-        //           text: "🤔 Ano nga uli ang 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮?",
-        //           buttons: [
-        //             {
-        //                 type: "postback",
-        //                 title: `Nagtuturo ng aral sa moral at espiritwal na aspeto`,
-        //                 payload: "CORRECT_PARABULA",
-        //             },
-        //             {
-        //                 type: "postback",
-        //                 title: `Nagtuturo ng pahayag na may kaugnay sa kasabihan`,
-        //                 payload: "WRONG_PARABULA",
-        //             },
-        //           ],
-        //         },
-        //       },
-        //     },
-        //   };
-        //   await callSendAPI(quizPayload);
-        // } 
-        // else if (payload === "CORRECT_PARABULA") {
-        //   await sendMessage(sender_psid, "✅ Tama!");
-        //   await new Promise((r) => setTimeout(r, 1500));
-
-        //   const followPayload = {
-        //     recipient: { id: sender_psid },
-        //     message: {
-        //       attachment: {
-        //         type: "template",
-        //         payload: {
-        //           template_type: "button",
-        //           text: `May tama ka!`,
-        //           buttons: [{ type: "postback", title: "OPO!", payload: "EXPLAIN_AGAIN" }],
-        //         },
-        //       },
-        //     },
-        //   };
-        //   await callSendAPI(followPayload);
-        // } 
-        // else if (payload === "WRONG_PARABULA") {
-        //   await sendMessage(sender_psid, "❌ Hmm. Sigurado ka ba?");
-        //   await new Promise((r) => setTimeout(r, 1500));
-
-        //   const followPayload = {
-        //     recipient: { id: sender_psid },
-        //     message: {
-        //       attachment: {
-        //         type: "template",
-        //         payload: {
-        //           template_type: "button",
-        //           text: "🤓 Upang mas maunawaan mo ang ating talakayan, gusto mo bang ipaliwanag ko uli sayo ang ating talakayan?",
-        //           buttons: [{ type: "postback", title: "OPO!", payload: "EXPLAIN_AGAIN" }],
-        //         },
-        //       },
-        //     },
-        //   };
-        //   await callSendAPI(followPayload);
-        // } 
-        // else if (payload === "EXPLAIN_AGAIN") {
-        //   await sendMessage(sender_psid, "✅ Okay, sige!");
-        }
+        } else if (payload === "UNDERSTOOD_PARABULA") {
+          await sendNaunawaan(sender_psid);
+        } else if (payload === "UNDERSTOOD2_PARABULA") {
+            await sendNextNaunawaan(sender_psid);
+        } else if (payload === "NOTUNDERSTOOD2_PARABULA") {
+            await sendNotNaunawaan(sender_psid);
+        } 
+        // else if (payload === "OPO_READ") {
+        //     await sendParabulaPagbasa(sender_psid);
+        // } else if (payload === "NABASA_NA") {
+        //     await sendNabasaNa(sender_psid);
+        // }
       }
     }
 
@@ -139,21 +88,21 @@ app.post("/webhook", async (req, res) => {
     res.sendStatus(404);
   }
 });
+//---------------------------------------------------------------------//
 
+
+//--------------------starting the lesson-----------------------//
 // intro message with button
 async function sendIntro(psid) {
-  try {
-    const userResponse = await fetch(
-      `https://graph.facebook.com/${psid}?fields=first_name&access_token=${PAGE_ACCESS_TOKEN}`
-    );
-    const userData = await userResponse.json();
-    const firstName = userData.first_name || "Mag-aaral";
 
-    const introText = `👋 Kumusta, ${firstName}!\n\n🤓 Ako si 𝗦𝗶𝗿 𝗚𝗹𝗲𝗻 𝗢𝗹𝗶𝘃𝗲𝗿 o mas kilala bilang si 𝗦𝗶𝗿 𝗚𝗼, ang iyong Filipino ChatBot. Ngayon ay magsisimula na tayo sa ating bagong aralin para sa ikatlong markahan sa unang sesyon sa Filipino.\n\n🥰 Panibagong talakayan, dagdag kaalaman!`;
-
+    const intro1 = `𝗙𝟵𝗣𝗧-𝗜𝗜𝗜𝗮-𝟱𝟬`;
+    const intro2 = `Nabibigyang-kahulugan ang matatalinghagang pahayag sa parabula`;
+    const introText = `👋 Kumusta!\n\n🤓 Ako si 𝗦𝗶𝗿 𝗚𝗹𝗲𝗻 𝗢𝗹𝗶𝘃𝗲𝗿 o mas kilala bilang si 𝗦𝗶𝗿 𝗚𝗼, ang iyong Filipino ChatBot. Ngayon ay magsisimula na tayo sa ating bagong aralin para sa ikatlong markahan sa unang sesyon sa Filipino.\n\n🥰 Panibagong talakayan, dagdag kaalaman!`;
     const learnText = `🤓 Handa ka na bang matuto?`;
 
-    // send intro then separate learn question
+    await sendMessage(psid, intro1);
+    await sendMessage(psid, intro2);
+    await new Promise((r) => setTimeout(r, 1000));
     await sendMessage(psid, introText);
     await new Promise((r) => setTimeout(r, 1000));
 
@@ -170,38 +119,25 @@ async function sendIntro(psid) {
         },
       },
     };
-
     await callSendAPI(payload);
-  } catch (error) {
-    console.error("Error fetching user info or sending intro:", error);
-  }
 }
 
-// after clicking first OPO
-async function sendReadyMessage(psid) {
-  try {
-    const userResponse = await fetch(
-      `https://graph.facebook.com/${psid}?fields=first_name&access_token=${PAGE_ACCESS_TOKEN}`
-    );
-    const userData = await userResponse.json();
-    const firstName = userData.first_name || "Mag-aaral";
 
-    const text1 = `😄 Ayan! Handa na nga si Bb./G. ${firstName}!`;
-    const text2 = `😄 Ngayon, bago tayo magsimula sa ating pormal na talakayan ay magkakaroon muna tayong paunang gawain.\n\n🤓 Tinatawag ko itong "𝗣𝗨𝗡𝗔𝗡 𝗔𝗧 𝗛𝗨𝗟𝗔𝗔𝗡", na kung saan kinakailangan mong mahulaan ang mga larawan na iyong makikita at may mga patlang na iyong pupunan upang makabuo ng isang salita.`;
+// INTRODUCTION ng ice breaker
+async function sendReadyMessage(psid) {
+
+    const text1 = `😄 Ayan! Handa na nga siya!`;
+    const text2 = `😄 Ngayon, bago tayo magsimula sa ating pormal na talakayan ay magkakaroon muna tayong paunang gawain.`;
+    const text3 = `🤓 Tinatawag ko itong “𝗣𝗨𝗡𝗔𝗡 𝗔𝗧 𝗛𝗨𝗟𝗔𝗔𝗡”, na kung saan kinakailangan mong mahulaan ang mga larawan na iyong makikita at may mga patlang na iyong pupunan upang makabuo ng isang salita.`;
 
     await sendMessage(psid, text1);
     await new Promise((r) => setTimeout(r, 1000));
     await sendMessage(psid, text2);
     await new Promise((r) => setTimeout(r, 1000));
-    await sendHandaKaNaBa(psid);
-  } catch (err) {
-    console.error("Error in sendReadyMessage:", err);
-  }
-}
+    await sendMessage(psid, text3);
+    await new Promise((r) => setTimeout(r, 1000));
 
-// handa ka na ba ulit
-async function sendHandaKaNaBa(psid) {
-  const payload = {
+    const payload = {
     recipient: { id: psid },
     message: {
       attachment: {
@@ -230,18 +166,20 @@ async function sendQuestion(psid, number) {
   let question = "";
 
   if (number === 1) {
+    await sendMessage(psid, "Sa unang larawan, ano ang iyong napansin at ano ang iyong sagot");
     await sendImage(psid, "https://i.imgur.com/rvx4L1e.jpg");
-    question = "Sa unang larawan, ano ang iyong napansin at ano ang iyong sagot?\n\nB \u200B_ \u200BB \u200BL \u200B_ \u200B_ \u200BA";
+    question = "\n\nB \u200B_ \u200BB \u200BL \u200B_ \u200B_ \u200BA";
   } else if (number === 2) {
     await sendMessage(psid, "Sumunod?");
     await new Promise((r) => setTimeout(r, 1000));
     await sendImage(psid, "https://i.imgur.com/gkt7Kr9.jpg");
     question = "G \u200B_ \u200BS \u200BT \u200B_ \u200B_ \u200BO \u200B_";
   } else if (number === 3) {
+    await sendMessage(psid, "Ikatlong larawan.");
     await sendImage(psid, "https://i.imgur.com/gUk0MqT.jpg");
-    question = "Ikatlong larawan.\n\n\u200B_ \u200BA \u200B_ \u200BA \u200BS \u200BL \u200BA \u200B_ \u200B_ \u200BN";
+    question = "\n\n\u200B_ \u200BA \u200B_ \u200BA \u200BS \u200B_ \u200BL \u200BA \u200B_ \u200B_ \u200BN";
   } else {
-    await sendMessage(psid, `🥰 Ayan! Maraming salamat, Bb./G. sa pagsagot.`);
+    await sendMessage(psid, `🥰 Ayan! Maraming salamat sa pagsagot!`);
     await new Promise((r) => setTimeout(r, 1500));
 
     // start parabula
@@ -253,7 +191,7 @@ async function sendQuestion(psid, number) {
           payload: {
             template_type: "button",
             text: "Kung makikita, ang ating magiging talakayan ay patungkol sa??",
-            buttons: [{ type: "postback", title: "SAAN PO?", payload: "SAAN_PO" }],
+            buttons: [{ type: "postback", title: "PATUNGKOL PO SAAN?", payload: "SAAN_PO" }],
           },
         },
       },
@@ -266,15 +204,16 @@ async function sendQuestion(psid, number) {
 
   await sendMessage(psid, question);
 }
+//end of icebreaker
 
 // parabula lesson
 async function sendParabulaLesson(psid) {
 
     const parabula1 = "🤓 Ang ating magiging talakayan ay patungkol sa 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮!";
     const parabula2 = "🧐 𝗦𝗶𝗿 𝗚𝗼, ano po ba ang 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮?";
-    const parabula3 = `📖 Ang 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮 ay isang maikling kuwento na nagtuturo ng 𝗮𝗿𝗮𝗹 𝘀𝗮 𝗺𝗼𝗿𝗮𝗹 𝗮𝘁 𝗲𝘀𝗽𝗶𝗿𝗶𝘁𝘄𝗮𝗹 𝗻𝗮 𝗮𝘀𝗽𝗲𝘁𝗼 𝗻𝗴 𝗯𝘂𝗵𝗮𝘆. Karaniwang ito ay batay sa mga aral ni Hesus mula sa Bibliya, ngunit maaari rin itong gamitin sa mas malawak na konteksto bilang kuwentong may 𝘁𝗮𝗹𝗶𝗻𝗵𝗮𝗴𝗮 𝗼 𝘀𝗶𝗺𝗯𝗼𝗹𝗶𝘀𝗺𝗼 na nagtuturo ng mabuting asal.`
-    const parabula4 = `📖 Mula ito sa salitang 𝗴𝗿𝗶𝘆𝗲𝗴𝗼 na “𝗽𝗮𝗿𝗮𝗯𝗼𝗹𝗲” na ang ibig sabihin ay 𝗽𝗮𝗴𝘁𝘂𝘁𝘂𝗹𝗮𝗱 𝗼 𝗽𝗮𝗴𝗵𝗮𝗵𝗮𝗺𝗯𝗶𝗻𝗴. Ibig sabihin, sa parabula ay may isang kuwento na ginagawang halimbawa upang ipaliwanag ang mas malalim na katotohanan.`
-    const parabula5 = `📖 Karaniwan, ang mga tauhan ay tao at ang mga pangyayari ay may malalim na kahulugang espiritwal.`
+    const parabula3 = `📖 Ang 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮 ay isang maikling kuwento na nagtuturo ng 𝗮𝗿𝗮𝗹 𝘀𝗮 𝗺𝗼𝗿𝗮𝗹 𝗮𝘁 𝗲𝘀𝗽𝗶𝗿𝗶𝘁𝘄𝗮𝗹 𝗻𝗮 𝗮𝘀𝗽𝗲𝘁𝗼 𝗻𝗴 𝗯𝘂𝗵𝗮𝘆. Karaniwang ito ay batay sa mga aral ni Hesus mula sa Bibliya, ngunit maaari rin itong gamitin sa mas malawak na konteksto bilang kuwentong may 𝘁𝗮𝗹𝗶𝗻𝗵𝗮𝗴𝗮 𝗼 𝘀𝗶𝗺𝗯𝗼𝗹𝗶𝘀𝗺𝗼 na nagtuturo ng mabuting asal.`;
+    const parabula4 = `📖 Mula ito sa salitang 𝗴𝗿𝗶𝘆𝗲𝗴𝗼 na “𝗽𝗮𝗿𝗮𝗯𝗼𝗹𝗲” na ang ibig sabihin ay 𝗽𝗮𝗴𝘁𝘂𝘁𝘂𝗹𝗮𝗱 𝗼 𝗽𝗮𝗴𝗵𝗮𝗵𝗮𝗺𝗯𝗶𝗻𝗴. Ibig sabihin, sa parabula ay may isang kuwento na ginagawang halimbawa upang ipaliwanag ang mas malalim na katotohanan.`;
+    const parabula5 = `📖 Karaniwan, ang mga tauhan ay tao at ang mga pangyayari ay may malalim na kahulugang espiritwal.`;
 
     await sendMessage(psid, parabula1);
     await new Promise((r) => setTimeout(r, 1000));
@@ -294,8 +233,8 @@ async function sendParabulaLesson(psid) {
         type: "template",
         payload: {
           template_type: "button",
-          text: "😌 Nauunawaan ba, Bb./G.?",
-          buttons: [{ type: "postback", title: "OPO!", payload: "UNDERSTOOD" }],
+          text: "😌 Nauunawaan ba?",
+          buttons: [{ type: "postback", title: "OPO!", payload: "UNDERSTOOD_PARABULA" }],
         },
       },
     },
@@ -304,6 +243,163 @@ async function sendParabulaLesson(psid) {
   await callSendAPI(understoodPayload);
 }
 
+//nauunawaan with opinionated answer
+async function sendNaunawaan(psid) {
+    
+    const nauunawaan1 = `✅ Okay, sige!`;
+    const nauunawaan2 = `🤔 Kung talagang nauunawaan mo. Ano nga uli ang 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮?\n\n(Ipahayag ang sagot.)`;
+
+    await sendMessage(psid, nauunawaan1);
+    await new Promise((r) => setTimeout(r, 1000));
+    await sendMessage(psid, nauunawaan2);
+    await new Promise((r) => setTimeout(r, 1000));
+
+    userProgress[psid] = "WAITING_OPINIONATED_ANSWER";
+}
+
+// handle opinionated response
+async function handleOpinionatedAnswer(psid) {
+    await sendMessage(psid, "✅ Ayan! Mahusay!");
+    await new Promise((r) => setTimeout(r, 1000));
+    await sendMessage(psid, "📖 Sa madaling sabi, ito ay kuwentong may aral na nagtuturo ng mabuting asal at pananampalataya.");
+    await new Promise((r) => setTimeout(r, 1000));
+    await sendMessage(psid, "📖 Dagdag pa na ang parabula ay isinusulat upang magturo, hindi lang para maglibang at magbigay aliw.");
+
+    const understood2Payload = {
+    recipient: { id: psid },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "🤓 Nauunawaan ba?",
+          buttons: [
+            { type: "postback", title: "OPO!", payload: "UNDERSTOOD2_PARABULA" },
+            { type: "postback", title: "HINDI PO.", payload: "NOTUNDERSTOOD2_PARABULA" }
+          ],
+        },
+      },
+    },
+  };
+
+  await callSendAPI(understood2Payload);
+}
+
+//hindi nauunawaan
+async function sendNotNaunawaan(psid) {
+
+    const notnicemsg = `😌 Okay, sige! Balikan natin`;
+    const explain1 = `📖 Ang 𝗽𝗮𝗿𝗮𝗯𝘂𝗹𝗮 ay isang maikling kuwento na nagtuturo ng 𝗮𝗿𝗮𝗹 𝘀𝗮 𝗺𝗼𝗿𝗮𝗹 𝗮𝘁 𝗲𝘀𝗽𝗶𝗿𝗶𝘁𝘄𝗮𝗹 𝗻𝗮 𝗮𝘀𝗽𝗲𝘁𝗼 𝗻𝗴 𝗯𝘂𝗵𝗮𝘆.`;
+    const explain2 = `📖 Mula ito sa salitang 𝗴𝗿𝗶𝘆𝗲𝗴𝗼 na “𝗽𝗮𝗿𝗮𝗯𝗼𝗹𝗲” na ang ibig sabihin ay pagtutulad o paghahambing.`;
+
+    await sendMessage(psid, notnicemsg);
+    await new Promise((r) => setTimeout(r, 1000));
+    await sendMessage(psid, explain1);
+    await new Promise((r) => setTimeout(r, 1000));
+    await sendMessage(psid, explain2);
+    await new Promise((r) => setTimeout(r, 1000));
+
+    const understood2Payload = {
+    recipient: { id: psid },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "🤓 Nauunawaan na ba?",
+          buttons: [
+            { type: "postback", title: "OPO!", payload: "UNDERSTOOD2_PARABULA" },
+            { type: "postback", title: "HINDI PO.", payload: "NOTUNDERSTOOD2_PARABULA" }
+          ],
+        },
+      },
+    },
+  };
+
+  await callSendAPI(understood2Payload);
+}
+
+//kung nauunawaan
+async function sendNextNaunawaan(psid) {
+    const nicemsg = `🤓 Tunay ngang nauunawaan!`;
+    const pagbasa = `📖 Ngayon ay magbabasa tayo at unaawain natin ang isang parabulang pinamagatang "𝗔𝗻𝗴 𝗔𝗹𝗶𝗯𝘂𝗴𝗵𝗮𝗻𝗴 𝗔𝗻𝗮𝗸" na matatagpuan sa sangunian ng bibliya sa ebanghelyo ni San Lucas sa kabanata 15, talata 11 hanggang 32 (Luke 15:11–32). `;
+
+    await sendMessage(psid, nicemsg);
+    await new Promise((r) => setTimeout(r, 1000));
+    await sendMessage(psid, pagbasa);
+    await new Promise((r) => setTimeout(r, 1000));
+
+    const pagbasaPayload = {
+    recipient: { id: psid },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "🤔 Handa na bang magbasa?",
+          buttons: [
+            { type: "postback", title: "OPO!", payload: "OPO_READ" },
+          ],
+        },
+      },
+    },
+  };
+
+  await callSendAPI(pagbasaPayload);
+}
+
+// async function sendParabulaPagbasa(psid) {
+
+//     const pagbasa1 = `May isang ama na may dalawang anak. Isang araw, hiningi ng bunsong anak ang kanyang mamanahin at lumayo sa kandungan ng ama upang makipagsapalaran sa malayong lupain. Doon ay nilustay niya ang yaman ng kanyang kabataan sa magarbo at walang saysay na pamumuhay.`;
+//     const pagbasa2 = `Nang maubos ang kanyang kayamanan, bumagsak siya sa laylayan ng kahirapan. Gutom, pagod, at walang maasahan, napilitan siyang magtrabaho sa bukid upang mag-alaga ng baboy. Sa gitna ng kanyang pagdurusa, nagliwanag ang kanyang diwa at naantig ang kanyang puso sa pagnanais na bumalik sa kanyang ama.`;
+//     const pagbasa3 = `Pag-uwi niya, malayo pa’y sinalubong siya ng ama na may yakap ng kapatawaran. Ipinagbunyi ng ama ang kanyang pagbabalik — isinuot sa kanya ang kasuotan ng dangal, isinukbit ang singsing ng pagtanggap, at isinapatos ang pagbangon mula sa pagkadusta.`;
+//     const pagbasa4 = `Ngunit nagdilim ang loob ng panganay na anak, sapagkat inakala niyang hindi siya pinahalagahan. Ipinaliwanag ng ama na dapat silang magsaya sapagkat ang anak na minsang naligaw ay muling natagpuan, at ang dating patay sa kasalanan ay muling nabuhay sa kabutihan.`;
+
+//     await sendMessage(psid, pagbasa1);
+//     await new Promise((r) => setTimeout(r, 3000));
+//     await sendMessage(psid, pagbasa2);
+//     await new Promise((r) => setTimeout(r, 9000));
+//     await sendMessage(psid, pagbasa3);
+//     await new Promise((r) => setTimeout(r, 9000));
+//     await sendMessage(psid, pagbasa4);
+//     await new Promise((r) => setTimeout(r, 9000));
+
+//   const nabasaPayload = {
+//     recipient: { id: psid },
+//     message: {
+//       attachment: {
+//         type: "template",
+//         payload: {
+//           template_type: "button",
+//           text: "☺️ Nabasa mo na ang parabula?",
+//           buttons: [{ type: "postback", title: "OPO!", payload: "NABASA_NA" }],
+//         },
+//       },
+//     },
+//   };
+
+//   await callSendAPI(nabasaPayload);
+// }
+
+// async function sendNabasaNa(psid) {
+//   const done = `wow`;
+
+//     await sendMessage(psid, done);
+//     await new Promise((r) => setTimeout(r, 1000));
+// }
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------//
+//-------------------FUNCTIONS----------------------------------------//
 // handle answers icebreaker
 async function handleUserAnswer(psid, userMessage) {
   if (!userProgress[psid]) return; // not icebreaker mode
@@ -375,3 +471,4 @@ async function callSendAPI(payload) {
 
 // start server
 app.listen(3000, () => console.log("Server running on port 3000"));
+//---------------------------------------------------------------------//
